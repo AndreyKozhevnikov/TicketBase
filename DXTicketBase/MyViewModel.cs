9ef0015081db729d328dc24c36577fb301b0b7d2
kozhevnikov.andrey <kozhevnikov.andrey@KOZHEVNIKOV-W10>
2017-11-08 09:53:08 +0300

    namespaces in files

--- DXTicketBase/MyViewModel.cs
+++ DXTicketBase/MyViewModel.cs
@@ -425,6 +425,8 @@ namespace DXTicketBase {
             fileList.Add(@"\{0}.Module\Welcome.html");
             fileList.Add(@"\{0}.Module\BusinessObjects\Contact.cs");
             fileList.Add(@"\{0}.Module\BusinessObjects\MyTask.cs");
+            fileList.Add(@"\{0}.Module\BusinessObjects\CustomClasses.cs");
+            fileList.Add(@"\{0}.Module\Controllers\CustomControllers.cs");
             fileList.Add(@"\{0}.Module\DatabaseUpdate\Updater.cs");
             if(IsWinAttached) {
                 fileList.Add(@"\{0}.Module.Win\{0}.Module.Win.csproj");
