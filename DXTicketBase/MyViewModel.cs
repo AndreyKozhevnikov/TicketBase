68fc00a3a98aadc135c805b08c4bbc799a9dc668
kozhevnikov.andrey <kozhevnikov.andrey@KOZHEVNIKOV-W10>
2019-06-14 17:48:40 +0300

    copy gitignore

--- DXTicketBase/MyViewModel.cs
+++ DXTicketBase/MyViewModel.cs
@@ -393,6 +393,7 @@ namespace DXTicketBase {
             File.Copy(Path.Combine(dropBoxPath, @"work\templates\MainSolution\delbinobj.bat"), Path.Combine(finalSolutionFolderPath, "delbinobj.bat"));
             var gitBatchFile = Path.Combine(finalSolutionFolderPath, "createGit.bat");
             File.Copy(Path.Combine(dropBoxPath, @"work\templates\MainSolution\createGit.bat"), gitBatchFile);
+            File.Copy(Path.Combine(dropBoxPath, @"work\templates\MainSolution\.gitignore"), Path.Combine(finalSolutionFolderPath, ".gitignore"));
             string solutionPath = "";
             string slnPathWithProjectName = "";
             List<string> filesWithSolutionName = new List<string>();
