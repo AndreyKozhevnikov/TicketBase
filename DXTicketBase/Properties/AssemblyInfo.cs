82bf696ac7d13a24c187dba3ef58d7b04b67b72a
kozhevnikov.andrey <kozhevnikov.andrey@KOZHEVNIKOV-W10>
2020-11-24 12:52:42 +0300

    update version

--- DXTicketBase/Properties/AssemblyInfo.cs
+++ DXTicketBase/Properties/AssemblyInfo.cs
@@ -42,6 +42,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("20.10.16.0535")]
-[assembly: AssemblyVersion("20.10.16.0535")]
+// [assembly: AssemblyVersion("20.11.24.1250")]
+[assembly: AssemblyVersion("20.11.24.1250")]
 
