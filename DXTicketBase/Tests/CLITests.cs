096ef7a07047d79e9858807ed821fc43bf90a94d
kozhevnikov.andrey <kozhevnikov.andrey@KOZHEVNIKOV-W10>
2024-09-13 15:42:51 +0400

    test

--- DXTicketBase/Tests/CLITests.cs
+++ DXTicketBase/Tests/CLITests.cs
@@ -74,8 +74,28 @@ namespace DXTicketBase.Tests {
             //assert
 
             Assert.AreEqual(@"dotnet new dx.xaf -p Blazor Win -n MyTestSolution232 -o ""c:\!Tickets\T1246029 How to manage the reset passwor\MyTestSolution232"" -orm XPO --multitenancy true -api Standalone --modules Reports Office", command);
+        }
 
+        [Test]
+        public void CLITestBase_WebAPISepar_WithMultiTenant() {
+            //arrange
+            var data = new DataForSolution();
+            data.Name = "MyTestSolution232";
+            data.FolderName = "c:\\!Tickets\\T1246029 How to manage the reset passwor\\";
+            data.HasWebAPISeparate = true;
+            data.HasSecurity = false;
+            data.HasMultitenant = true;
+            data.Modules.Add(ModulesEnum.Reports);
+            data.Modules.Add(ModulesEnum.Office);
 
+            //act
+
+            var builder = new CLIBuilder();
+            var command = builder.GetCLIString(data);
+
+            //assert
+
+            Assert.AreEqual(@"dotnet new dx.xaf -p Blazor Win -n MyTestSolution232 -o ""c:\!Tickets\T1246029 How to manage the reset passwor\MyTestSolution232"" -orm XPO --multitenancy true -api Standalone --modules Reports Office", command);
         }
     }
 }
